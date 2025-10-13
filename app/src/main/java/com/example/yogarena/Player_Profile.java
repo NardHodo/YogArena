992e886edbac9813e1471a8f2c5a27f9d8b459fa
NardHodo <133963030+NardHodo@users.noreply.github.com>
2025-10-13 18:07:51 +0800

    - lipat laptop

---
+++ app/src/main/java/com/example/yogarena/Player_Profile.java
@@ -0,0 +1,13 @@
+package com.example.yogarena;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Player_Profile extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_profile);
+    }
+}
